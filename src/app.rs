bde7d75f0838c2b93d023e59b07eacefc78b53c9
Antoine Gourlay <antoine@gourlay.fr>
2020-04-02 14:40:36 +0200

    refactoring: separate everything into modules

---
+++ src/app.rs
@@ -0,0 +1,102 @@
+use anyhow::*;
+use crate::usb;
+use gio::prelude::*;
+use gtk::prelude::*;
+use gtk_macros::*;
+
+pub struct Application {
+    app: gtk::Application,
+}
+
+impl Application {
+    pub fn new() -> Result<Application> {
+        let app = gtk::Application::new(
+            Some("net.gourlaysama.gpower-tweaks"),
+            gio::ApplicationFlags::default(),
+        )?;
+        app.connect_activate(|app| {
+            let provider = gtk::CssProvider::new();
+            provider
+                .load_from_data(include_bytes!("../data/ui/shell.css"))
+                .unwrap();
+            gtk::StyleContext::add_provider_for_screen(
+                &gdk::Screen::get_default().unwrap(),
+                &provider,
+                gtk::STYLE_PROVIDER_PRIORITY_USER,
+            );
+            let builder = gtk::Builder::new_from_string(include_str!("../data/ui/window.ui"));
+            get_widget!(builder, gtk::ApplicationWindow, win);
+            get_widget!(builder, gtk::ListBox, category_list);
+            let label = gtk::Label::new_with_mnemonic(Some("_USB Autosuspend"));
+            label.set_margin_top(6);
+            label.set_margin_bottom(6);
+            label.set_margin_start(6);
+            label.set_margin_end(6);
+            let row = gtk::ListBoxRow::new();
+            row.add(&label);
+            category_list.add(&row);
+            // let label = gtk::Label::new_with_mnemonic(Some("_PCI Runtime Management"));
+            // label.set_margin_top(6);
+            // label.set_margin_bottom(6);
+            // label.set_margin_start(6);
+            // label.set_margin_end(6);
+            // let row = gtk::ListBoxRow::new();
+            // row.add(&label);
+            // category_list.add(&row);
+            get_widget!(builder, gtk::ListBox, main_list_box);
+            let mut entries = Vec::new();
+            for d in usb::list_devices().unwrap() {
+                entries.push(build_usb_entry(&d));
+            }
+            for e in entries {
+                main_list_box.add(&e);
+            }
+            win.set_application(Some(app));
+            win.show_all();
+        });
+
+        Ok(Application { app })
+    }
+
+    pub fn run(&self) {
+        self.app.run(&std::env::args().collect::<Vec<_>>());
+    }
+}
+
+
+fn build_usb_entry(device: &usb::UsbDevice) -> gtk::ListBoxRow {
+    let row = gtk::ListBoxRow::new();
+    row.set_activatable(false);
+    row.set_selectable(false);
+    let main_box = gtk::Box::new(gtk::Orientation::Horizontal, 12);
+
+    let image = gtk::Image::new_from_icon_name(Some("input-mouse"), gtk::IconSize::Dialog);
+    main_box.add(&image);
+
+    let text_box = gtk::Box::new(gtk::Orientation::Vertical, 0);
+    let label_main = gtk::Label::new(Some(&device.get_name()));
+    let label_info = gtk::Label::new(Some(&device.get_description()));
+    label_info.get_style_context().add_class("desc_label");
+    text_box.add(&label_main);
+    text_box.add(&label_info);
+    text_box.set_valign(gtk::Align::Center);
+    main_box.pack_start(&text_box, true, true, 0);
+
+    let button = gtk::Switch::new();
+    main_box.add(&button);
+
+    let cb_box = gtk::ComboBoxText::new_with_entry();
+    cb_box.set_valign(gtk::Align::Center);
+    cb_box.append_text("Immediately");
+    cb_box.set_active(Some(0));
+    cb_box.append_text("1 second");
+    cb_box.append_text("2 seconds");
+    cb_box.append_text("5 seconds");
+    cb_box.append_text("20 seconds");
+    cb_box.append_text("1 minute");
+    cb_box.append_text("5 minutes");
+    main_box.add(&cb_box);
+
+    row.add(&main_box);
+    row
+}
---
+++ src/usb/mod.rs
@@ -0,0 +1,152 @@
+mod db;
+
+use anyhow::*;
+use db::parse_all;
+use std::collections::HashMap;
+use std::fs;
+use std::os::linux::fs::*;
+use std::path::PathBuf;
+
+pub struct UsbDevice {
+    char_device_path: PathBuf,
+    vendor_id: Option<u32>,
+    product_id: Option<u32>,
+    device_class: Option<u16>,
+    db_vendor_name: Option<String>,
+    db_product_name: Option<String>,
+    product_name: Option<String>,
+    manufacturer_name: Option<String>,
+}
+
+impl UsbDevice {
+    fn from(char_device_path: PathBuf) -> UsbDevice {
+        UsbDevice {
+            char_device_path,
+            vendor_id: None,
+            product_id: None,
+            device_class: None,
+            db_vendor_name: None,
+            db_product_name: None,
+            product_name: None,
+            manufacturer_name: None,
+        }
+    }
+
+    pub fn get_name(&self) -> String {
+        let mut desc = String::new();
+        if let Some(vendor) = self.db_vendor_name.as_ref().or_else(|| self.manufacturer_name.as_ref()) {
+            desc.push_str(&vendor);
+            desc.push(' ');
+        }
+        if let Some(product) = self.db_product_name.as_ref().or_else(|| self.product_name.as_ref()) {
+            desc.push_str(&product);
+        } else if let Some(9) = self.device_class  {
+            desc.push_str("Hub");
+        }
+
+        desc
+    }
+
+    pub fn get_description(&self) -> String {
+        let mut desc = String::new();
+
+        if self.db_vendor_name.is_some() {
+            if let Some(manufacturer_name) = self.manufacturer_name.as_ref() {
+                desc.push_str(&manufacturer_name);
+                desc.push(' ');
+            }
+        }
+        if self.db_product_name.is_some() {
+            if let Some(product_name) = self.product_name.as_ref() {
+                desc.push_str(&product_name);
+            }
+        }
+
+        if desc.is_empty() {
+            desc = format!("{}", self.char_device_path.display());
+        }
+
+        desc
+    }
+}
+
+pub fn list_devices() -> Result<Vec<UsbDevice>> {
+    let db = make_usb_db()
+        .map_err(|e| {
+            eprintln!("Ignoring error parting db: {}", e);
+        })
+        .ok();
+
+    let mut devices = Vec::new();
+
+    for entry in std::fs::read_dir("/dev/bus/usb/")? {
+        if let Ok(entry) = entry {
+            if let Ok(tpe) = entry.file_type() {
+                if tpe.is_dir() {
+                    for entry in entry.path().read_dir()? {
+                        if let Ok(entry) = entry {
+                            let dev = make_device(entry, db.as_ref())?;
+                            devices.push(dev);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(devices)
+}
+
+fn make_device(
+    entry: std::fs::DirEntry,
+    usb_db: Option<&HashMap<u32, db::Vendor>>,
+) -> Result<UsbDevice> {
+    let m = entry.metadata()?;
+    let rdev = m.st_rdev().to_ne_bytes();
+    let major = rdev[1];
+    let minor = rdev[0] as u16;
+    let minor = minor + ((rdev[2] as u16) << 4);
+
+    let char_path = std::path::Path::new("/sys/dev/char/").join(format!("{}:{}", major, minor));
+    let vendor_path = char_path.join("idVendor");
+    let product_path = char_path.join("idProduct");
+    let product_name_path = char_path.join("product");
+    let class_path = char_path.join("bDeviceClass");
+
+    let mut usb_device = UsbDevice::from(char_path);
+
+    if let Ok(vendor) = fs::read_to_string(&vendor_path) {
+        let vendor_id = u32::from_str_radix(&vendor.trim(), 16)?;
+        usb_device.vendor_id = Some(vendor_id);
+        if let Some(vendor) = usb_db.and_then(|db| db.get(&vendor_id)) {
+            usb_device.db_vendor_name = Some(vendor.name.trim().to_string());
+
+            if let Ok(product) = fs::read_to_string(&product_path) {
+                let product_id = u32::from_str_radix(&product.trim(), 16)?;
+                usb_device.product_id = Some(product_id);
+                if let Some(product) = vendor.devices.get(&product_id) {
+                    usb_device.db_product_name = Some(product.trim().to_string());
+                }
+            }
+        }
+    }
+
+    if let Ok(product_name) = fs::read_to_string(&product_name_path) {
+        usb_device.product_name = Some(product_name.trim().to_owned());
+    }
+
+    if let Ok(class_str) = fs::read_to_string(&class_path) {
+        if let Ok(class_id) = u16::from_str_radix(class_str.trim(), 16) {
+            usb_device.device_class = Some(class_id);
+        }
+    }
+
+    Ok(usb_device)
+}
+
+fn make_usb_db() -> Result<HashMap<u32, db::Vendor>> {
+    let db_content = std::fs::read("/usr/share/hwdata/usb.ids")?;
+    let (db_str, _, _) = encoding_rs::WINDOWS_1252.decode(&db_content);
+
+    parse_all(&db_str)
+}
